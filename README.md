3e0d7b8f91bb032ae3723c0e3d8bbf8ae76837fb
mcbeaniee <150880707+mcbeaniee@users.noreply.github.com>
2024-11-27 05:04:25 -0500

    made the project :3

--- README.md
+++ README.md
@@ -1,21 +1,18 @@
-# Template p5 Project
+# CART 214 - Interactive Infographic with pictograms
 
-Author Name
+Made by Jack McDOnlad
 
 [View this project online](URL_FOR_THE_RUNNING_PROJECT)
 
 ## Description
 
-This description should help the reader understand what the program is, what any controls do, and perhaps what the desired experience it.
+This is a website for displaying the infographic created with Aesprite and p5.js.
+This project was created for Joanna M. Berzowski's CART 214 class, Visual Form and Communication using resources and knowledge from Pippin Barr's CART 253 class.
 
 ## Credits
 
-This bit should describe what tools were used. For example:
-
-This project uses [p5.js](https://p5js.org).
+This project uses [p5.js](https://p5js.org) and HTML.
 
 ## Attribution
 
-This bit should attribute any assets or other elements used taken from other sources. For example:
-
-The fish images were sourced from the [Creative Commons image "Georgia Aquarium Fish"](https://search.creativecommons.org/photos/96f6f770-eac1-488c-8abb-16bee7bcc874) by Mike Johnston which is licensed with CC BY 2.0. To view a copy of this license, visit https://creativecommons.org/licenses/by/2.0/.
+All assets were created using Aesprite or Google Drawings.
--- js/script.js
+++ js/script.js
@@ -1,24 +1,76 @@
 /**
- * Title of Project
- * Author Name
+ * CART 214 Infographic
+ * Jack McDonald
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Jack's infographic
  */
 
 "use strict";
 
+//image lets
+let standardscene;
+let alarmscene;
+let teascene;
+let pcscene;
+let ramenscene;
+let alcoholscene;
+
+let alarmtext;
+let teatext;
+let pctext;
+let ramentext;
+let alcoholtext;
+
+//preload images
+
+function preload(){
+    //scene images
+    standardscene = loadImage('assets/images/defaultstate1.png');
+    alarmscene = loadImage('assets/images/clockstate.gif');
+    teascene = loadImage('assets/images/teastate.gif');
+    pcscene = loadImage('assets/images/pcstate.gif');
+    ramenscene = loadImage('assets/images/ramenstate.gif');
+    alcoholscene = loadImage('assets/images/alcoholstate.gif');
+    //text images
+    alarmtext = loadImage('assets/images/alarmtext.png');
+    teatext = loadImage('assets/images/teatext.png');
+    pctext = loadImage('assets/images/pctext.png');
+    ramentext = loadImage('assets/images/ramentext.png');
+    alcoholtext = loadImage('assets/images/alcoholtext.png');
+}
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * setup scene
 */
 function setup() {
-
+    createCanvas(1280,1280);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * draw based on state
 */
 function draw() {
-
-}
+    
+    if ((mouseX > 877) && (mouseX < 1035) &&
+    (mouseY > 755) && (mouseY < 834)){
+        image(ramenscene,0,150);
+        image(ramentext,885,388);
+    } else if((mouseX > 217) && (mouseX < 377) &&
+    (mouseY > 542) && (mouseY < 837))  {
+        image(pcscene,0,150);
+        image(pctext,200,220);
+    } else if((mouseX > 78) && (mouseX < 243) &&
+    (mouseY > 895) && (mouseY < 1106))  {
+        image(alcoholscene,0,150);
+        image(alcoholtext,290,325);
+    } else if((mouseX > 1196) && (mouseX < 1278) &&
+    (mouseY > 863) && (mouseY < 918))  {
+        image(alarmscene,0,150);
+        image(alarmtext,915,495);
+    } else if((mouseX > 398) && (mouseX < 480) &&
+    (mouseY > 766) && (mouseY < 838))  {
+        image(teascene,0,150);
+        image(teatext,78,400);
+    } else 
+        image(standardscene,0,150);
+} 
